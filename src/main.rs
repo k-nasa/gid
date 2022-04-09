5ee26ca8a929f7779fdff8b22de05d03bd2e6258
k-nasa <htilcs1115@gmail.com>
2022-04-09 20:36:55 +0900

    refactor internal structure

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,13 @@ pub type URI = String;
 )]
 pub struct IssueQuery;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.docs.graphql",
+    query_path = "graphql/query.graphql"
+)]
+pub struct TrackIssuesQuery;
+
 type IssueGraph = HashMap<i64, Vec<Issue>>;
 struct Issue {
     number: i64,
@@ -47,7 +54,7 @@ export GITHUB_ACCESS_TOKEN=xxxx"
 }
 
 async fn run(client: Client, args: CliArgs) -> Result<(), anyhow::Error> {
-    fetch_tracked_issue(
+    let issue_graph = fetch_tracked_issue(
         &client,
         args.issue_number as i64,
         &args.organization,
@@ -55,7 +62,7 @@ async fn run(client: Client, args: CliArgs) -> Result<(), anyhow::Error> {
     )
     .await?;
 
-    let mermaid = build_mermaid(todo!());
+    let mermaid = build_mermaid(issue_graph);
 
     println!("{}", mermaid);
 
@@ -63,31 +70,41 @@ async fn run(client: Client, args: CliArgs) -> Result<(), anyhow::Error> {
 }
 
 fn build_mermaid(issue_map: IssueGraph) -> String {
-    let head = r#"
-```mermaid
-graph LR
-    classDef CLOSED fill:#8256d0,color:#FFFFFF,stroke-width:0px;
-    classDef OPEN fill:#347d39,color:#FFFFFF,stroke-width:0px;
-```"#;
-
-    let mut body = String::new();
-    let mut links = String::new();
+    let mut body = vec![];
+    let mut links = vec![];
     for (number, issues) in issue_map {
         for issue in issues {
             let t = format!(
-                "\t{parent} --> {child}[\"{title}\"]:::{state}\n",
+                "{parent} --> {child}[\"{title}\"]:::{state}",
                 parent = number,
                 child = issue.number,
                 title = issue.title,
                 state = issue.state
             );
-            body += &t;
+            body.push(t);
 
-            links += &format!("{}\n", issue.url);
+            links.push(format!(
+                "click {} href \"{}\" _blank",
+                issue.number, issue.url
+            ));
         }
     }
 
-    format!("{}\n{}\n{}", head, body, links)
+    format!(
+        r#"
+```mermaid
+graph LR
+
+classDef CLOSED fill:#8256d0,color:#FFFFFF,stroke-width:0px;
+classDef OPEN fill:#347d39,color:#FFFFFF,stroke-width:0px;
+
+{body}
+
+{links}
+```"#,
+        body = body.join("\n"),
+        links = links.join("\n")
+    )
 }
 
 #[async_recursion]
@@ -96,16 +113,32 @@ async fn fetch_tracked_issue(
     root_issue: i64,
     owner: &str,
     repository: &str,
+) -> Result<IssueGraph, anyhow::Error> {
+    let mut init_state = HashMap::new();
+    _fetch_tracked_issue(client, root_issue, owner, repository, &mut init_state).await?;
+
+    return Ok(init_state);
+}
+
+#[async_recursion]
+async fn _fetch_tracked_issue(
+    client: &reqwest::Client,
+    root_issue: i64,
+    owner: &str,
+    repository: &str,
+    issue_graph: &mut IssueGraph,
 ) -> Result<(), anyhow::Error> {
-    let v = issue_query::Variables {
+    let v = track_issues_query::Variables {
         owner: owner.into(),
         repository_name: repository.into(),
         number: root_issue,
     };
-    let request_body = IssueQuery::build_query(v);
+    let request_body = TrackIssuesQuery::build_query(v);
 
     let res = client.post(GITHUB_URL).json(&request_body).send().await?;
-    let response_body: Response<issue_query::ResponseData> = res.json().await?;
+    let response_body: Response<track_issues_query::ResponseData> = res.json().await?;
+
+    // FIXME unwrap地獄を修正したい
     for i in response_body
         .data
         .unwrap()
@@ -120,18 +153,25 @@ async fn fetch_tracked_issue(
         let i = i.as_ref().unwrap();
 
         let state = match i.state {
-            issue_query::IssueState::OPEN => "OPEN",
-            issue_query::IssueState::CLOSED => "CLOSED",
+            track_issues_query::IssueState::OPEN => "OPEN",
+            track_issues_query::IssueState::CLOSED => "CLOSED",
             _ => "OTHER",
         };
 
-        println!(
-            "\t{} --> {}[\"{}\"]:::{}",
-            root_issue, i.number, i.title, state
-        );
+        let issue = Issue {
+            number: i.number,
+            url: i.url.clone().into(),
+            title: i.title.clone().into(),
+            state: state.into(),
+        };
 
-        println!("click {} href \"{}\" _blank", i.number, i.url);
-        fetch_tracked_issue(client, i.number, owner, repository).await?;
+        if let Some(issues) = issue_graph.get_mut(&root_issue) {
+            issues.push(issue);
+        } else {
+            issue_graph.insert(root_issue, vec![issue]);
+        }
+
+        _fetch_tracked_issue(client, i.number, owner, repository, issue_graph).await?;
     }
 
     Ok(())
